2af926e4fcce79d97875fb1ab5173ffc0493cc60
Vedant Bhagat <94466908+Vedant543@users.noreply.github.com>
2021-11-30 12:11:32 +0530

    Create for_loop.c

---
+++ for_loop.c
@@ -0,0 +1,61 @@
+int main()
+ {
+      int a,b,n,i;
+        scanf("%d %d",&a,&b);
+        for(i=a;i<=b;i++)
+            {
+                n=i;
+                if(1<=n && n<=9)
+                    {
+                    if(n==1)
+                        {
+                           printf("one\n");
+                        } 
+                    else if(n==2)
+                        {
+                            printf("two\n");
+                        }
+                    else if(n==3)
+                        {
+                             printf("three\n");
+                        }
+                    else if(n==4)
+                        {
+                            printf("four\n");
+                        }
+                    else if(n==5)
+                        {
+                            printf("five\n");
+                        }
+                    else if(n==6)
+                        {
+                            printf("six\n");
+                        }
+                    else if(n==7)
+                        {
+                           printf("seven\n");
+                        }
+                    else if(n==8)
+                        {
+                            printf("eight\n");
+                        } 
+                    else 
+                        {
+                             printf("nine\n");
+                        }
+                    }
+                    else if(n>9)
+                        {
+                            if(n%2==0)
+                                {
+                             printf("even\n");
+                                }
+                            else 
+                                {
+                             printf("odd\n");
+                                }
+                        }
+            }         
+    // Complete the code.
+    return 0;
+}
